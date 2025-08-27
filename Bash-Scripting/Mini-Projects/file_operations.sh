954d6f3fc11faefd8a549842933d5bc7383780d6
Elsa Adjei <elsaadjei55@gmail.com>
2025-08-27 12:45:20 +0100

    Update file_operations.sh

--- Bash-Scripting/Mini-Projects/file_operations.sh
+++ Bash-Scripting/Mini-Projects/file_operations.sh
@@ -5,3 +5,5 @@ cd operation
 touch test.txt
 echo "This is a test" > test.txt
 cat test.txt
+
+# This script creates a directory called 'operation'. It then navigates to that directory,creates a file in that directory, weites text and directs it into the file, then displays the contents of the file.
